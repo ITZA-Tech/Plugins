f927f86fa42881227e7b3da2c792181c17ee11e8
vitoo <victorhugomeurerdelgrandi@gmail.com>
2023-12-09 20:21:39 -0400

    feat: add mod.ts

--- island.ts
+++ island.ts
@@ -1,28 +1,28 @@
-import { Plugin } from '$fresh/server.ts'
-import fileUrl from 'npm:file-url'
-import { expandGlob } from 'std/fs/expand_glob.ts'
+import { Plugin } from "$fresh/server.ts";
+import fileUrl from "npm:file-url";
+import { expandGlob } from "std/fs/expand_glob.ts";
 
-const islands = new Set<string>()
-const denoJson = JSON.parse(Deno.readTextFileSync('deno.json'))
+const islands = new Set<string>();
+const denoJson = JSON.parse(Deno.readTextFileSync("deno.json"));
 
 for await (
-	const file of expandGlob('**/*.tsx', {
-		exclude: denoJson.exclude ?? [],
-	})
+  const file of expandGlob("**/*.tsx", {
+    exclude: denoJson.exclude ?? [],
+  })
 ) {
-	if (!file.isFile) throw new Error(`${file.path} is not a file`)
-	
-	if ((await Deno.readTextFile(file.path)).startsWith('\'use client\'')) {
-		islands.add(fileUrl(file.path))
-	}
+  if (!file.isFile) throw new Error(`${file.path} is not a file`);
+
+  if ((await Deno.readTextFile(file.path)).startsWith("'use client'")) {
+    islands.add(fileUrl(file.path));
+  }
 }
 
 export default function (): Plugin {
-	return {
-		name: '@island',
-		islands: {
-			baseLocation: '',
-			paths: [...islands],
-		},
-	}
+  return {
+    name: "@island",
+    islands: {
+      baseLocation: "",
+      paths: [...islands],
+    },
+  };
 }

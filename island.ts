69feb1cbc50c379b58beac7ec927d2e37c3aae94
vitoo <victorhugomeurerdelgrandi@gmail.com>
2023-12-11 07:20:35 -0400

    fix: use asynchronous methods

--- island.ts
+++ island.ts
@@ -1,33 +1,33 @@
 import { Plugin } from "$fresh/server.ts";
 import fileUrl from "npm:file-url";
 import { yellow } from "std/fmt/colors.ts";
-import { expandGlobSync } from "std/fs/expand_glob.ts";
+import { expandGlob } from "std/fs/expand_glob.ts";
 
-export default function islands(): Plugin {
-  const start = performance.now();
-  const islands = new Set<string>();
-  const denoJson = JSON.parse(Deno.readTextFileSync("deno.json"));
-  const decoder = new TextDecoder();
+const start = performance.now();
+const islands = new Set<string>();
 
-  const files = expandGlobSync("**/*.tsx", {
-    exclude: denoJson.exclude ?? [],
-  });
-  for (const file of files) {
-    const of = Deno.openSync(file.path);
-    const buffer = new Uint8Array(11);
-    Deno.readSync(of.rid, buffer);
-    if (decoder.decode(buffer.slice(1)) === "use client") {
-      islands.add(fileUrl(file.path));
-    }
-    Deno.close(of.rid);
+const denoJson = JSON.parse(await Deno.readTextFile("deno.json"));
+const decoder = new TextDecoder();
+const files = expandGlob("**/*.tsx", {
+  exclude: denoJson.exclude ?? [],
+});
+for await (const file of files) {
+  const of = await Deno.open(file.path);
+  const buffer = new Uint8Array(11);
+  await Deno.read(of.rid, buffer);
+  if (decoder.decode(buffer.slice(1)) === "use client") {
+    islands.add(fileUrl(file.path));
   }
+  Deno.close(of.rid);
+}
 
-  console.log(
-    yellow(
-      `\u26A1 Found ${islands.size} islands in ${Math.floor(performance.now() - start)}ms`,
-    ),
-  );
+console.log(
+  yellow(
+    `\u26A1 Found ${islands.size} islands in ${Math.floor(performance.now() - start)}ms`,
+  ),
+);
 
+export default function islandsPlugin(): Plugin {
   return {
     name: "@island",
     islands: {

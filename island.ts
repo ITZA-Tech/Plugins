2100cbec70d48a1f6dca2cedcd15952cc8623234
vitoo <victorhugomeurerdelgrandi@gmail.com>
2023-12-11 07:47:37 -0400

    refactor: read entire file

--- island.ts
+++ island.ts
@@ -7,23 +7,30 @@ const start = performance.now();
 const islands = new Set<string>();
 
 const denoJson = JSON.parse(await Deno.readTextFile("deno.json"));
-const decoder = new TextDecoder();
-const files = expandGlob("**/*.tsx", {
-  exclude: denoJson.exclude ?? [],
-});
-for await (const file of files) {
-  const of = await Deno.open(file.path);
-  const buffer = new Uint8Array(11);
-  await Deno.read(of.rid, buffer);
-  if (decoder.decode(buffer.slice(1)) === "use client") {
+// const decoder = new TextDecoder();
+for await (
+  const file of expandGlob("**/*.tsx", {
+    exclude: denoJson.exclude ?? [],
+  })
+) {
+  if ((await Deno.readTextFile(file.path)).match(/['"]use client['"]/)) {
     islands.add(fileUrl(file.path));
   }
-  Deno.close(of.rid);
+
+  // const of = await Deno.open(file.path);
+  // const buffer = new Uint8Array(11);
+  // await Deno.read(of.rid, buffer);
+  // if (decoder.decode(buffer.slice(1)) === "use client") {
+  //   islands.add(fileUrl(file.path));
+  // }
+  // Deno.close(of.rid);
 }
 
 console.log(
   yellow(
-    `\u26A1 Found ${islands.size} islands in ${Math.floor(performance.now() - start)}ms`,
+    `\u26A1 Found ${islands.size} islands in ${
+      Math.floor(performance.now() - start)
+    }ms`,
   ),
 );
 
